0b7b432494f35d3810e31ea85e6cbd136f1c05dd
theArgo15 <tony@tonywitt.com>
2022-09-06 11:07:44 -0500

    create loop for all loggers

--- loggerDownload.py
+++ loggerDownload.py
@@ -1,7 +1,6 @@
 import pyautogui
 import time
-def singleLogger():
-    loggerNumber = '19'
+def singleLogger(loggerNumber):
     #Click connect (890,470)
     pyautogui.click(890,470)
     time.sleep(.3)
@@ -11,10 +10,11 @@ def singleLogger():
     #click download box (858,586)
     pyautogui.click(858,586)
     #wait for download
+    print('Searching for completed status bar...')
     while True:
         px = pyautogui.pixel(1123, 356)
-        print(px)
         if px[0]==0:
+            print('Status bar full!')
             break
     time.sleep(.3) 
     #type Logger# ENTER
@@ -42,15 +42,23 @@ def singleLogger():
     pyautogui.typewrite(loggerName)
     #click setup (1066,707)
     pyautogui.click(1066,707)
-    time.sleep(.3)
+    time.sleep(1)
     #click Okay (954,586)
     pyautogui.click(954,586)
     time.sleep(.3)
     #click close (1503, 230)
-    x=input('Done?')
-    pyautogui.click(1503, 230)
-    time.sleep(.3)
+    # x=input('Done?')
+    # pyautogui.click(1503, 230)
+    # time.sleep(.3)
 def tester():
     pass
 if __name__ == "__main__":
-    singleLogger()
+    #loggerNumber = '5'
+    print('Do the first one manually for now')
+    for loggerNumber in range(1,20):
+        singleLogger(loggerNumber)
+        #click close (1503, 230)
+        x=input('Done?')
+        pyautogui.click(1503, 230)
+        if loggerNumber <19:
+            x=input(f'Load logger {loggerNumber+1} now:')
